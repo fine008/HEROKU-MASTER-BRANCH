ff355468315e93a381bd24141e557534f8f52ee0
fine008 <115710749+fine008@users.noreply.github.com>
2023-02-23 00:03:54 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -670,7 +670,7 @@ if len(BASE_URL) == 0:
 
 UPSTREAM_REPO = environ.get('UPSTREAM_REPO', '')
 if len(UPSTREAM_REPO) == 0:
-   UPSTREAM_REPO = 'https://github.com/weebzone/WZML'
+   UPSTREAM_REPO = 'https://github.com/fine008/HEROKU-MASTER-BRANCH'
 
 UPSTREAM_BRANCH = environ.get('UPSTREAM_BRANCH', '')
 if len(UPSTREAM_BRANCH) == 0:
